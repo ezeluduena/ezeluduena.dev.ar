034f5b4bec5d4fc487fc7653254299b6deb6ab88
ezequiel <ezeluduena123@gmail.com>
2024-08-20 16:21:16 -0300

    feat: home page translation

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,11 +5,21 @@ import Image from '~/components/image';
 import Link from '~/components/link';
 import Paragraph from '~/components/paragraph';
 import SocialLinks from '~/components/sociallinks';
+import useLocale from '~/hooks/useLocale';
+import homeTranslations from '~/public/locale/home';
 
 const HomePage: NextPage = () => {
+  const { locale, setLocale } = useLocale();
   const age = new Date(Date.now() - Date.parse('2000-12-06')).getUTCFullYear() - 1970;
 
   const famafLccUrl = 'https://www.famaf.unc.edu.ar/academica/grado/licenciatura-en-ciencias-de-la-computaci%C3%B3n/';
+
+  const t = homeTranslations[locale];
+
+  const handleLocaleChange = () => {
+    setLocale(locale === 'es' ? 'en' : 'es');
+  }
+
 
   return (
     <>
@@ -29,25 +39,23 @@ const HomePage: NextPage = () => {
 
         <div>
           <div className={c('text-center', 'md:text-left')}>
-            <Heading>Buenas! 👋</Heading>
+            <Heading>{t.greeting}</Heading>
+            <Paragraph>
+              {t.introduction.replace('{age}', age.toString())}
+              <br />
+              {t.study1} <Link href={famafLccUrl}> {t.study2} </Link>
+              {t.study3}
+            </Paragraph>
+            <Paragraph>
+              {t.interest1}
+              <br />
+              {t.interest2}
+            </Paragraph>
+            <Paragraph>
+              {t.projects1} <Link href={'/projects'}> {t.projects2}</Link> {t.projects3}
+              <Link href={'/blog'}>blog</Link>{t.projects4}
+            </Paragraph>
           </div>
-          <Paragraph>
-            Me llamo Ezequiel, tengo {age} años y soy de Córdoba, Argentina.
-            <br />
-            Soy un estudiante avanzado de <Link href={famafLccUrl}> Ciencias de la Computación </Link>
-            en la Universidad Nacional de Córdoba. Me gusta el fútbol, el cine y andar en bicicleta.
-          </Paragraph>
-          <Paragraph>
-            Cuando era chico me interesé en cómo funcionan los celulares y las computadoras.
-            Desde entonces que paso bastante tiempo investigando sobre tecnología en general.
-            <br />
-            Hoy mis principales intereses son la programación backend, el software libre y la administración de sistemas.
-          </Paragraph>
-          <Paragraph>
-            En esta página vas a encontrar los distintos <Link href={'/projects'}> proyectos</Link> en los que estuve trabajando.
-            También podés leer mi <Link href={'/blog'}> blog</Link>, donde escribo
-            para registrar y compartir lo que voy aprendiendo.
-          </Paragraph>
         </div>
       </section >
 
