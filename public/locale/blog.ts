b6c0153b46365e0e63cf08801dfa23a3c865eff0
Ezequiel Ludueña <ezeluduena123@gmail.com>
2025-07-09 16:45:21 -0300

    feat: tech sovereignty blog post

--- public/locale/blog.ts
+++ public/locale/blog.ts
@@ -8,7 +8,12 @@ const blogTranslations = {
         description3: " to get notified when I post a new article.",
         time: "min read",
         ezeluduena: "Creating a personal website",
+<<<<<<< Updated upstream
         la_cosa: "La Cosa: A web game backend development"
+=======
+        la_cosa: "La Cosa: A web game backend development",
+        sob_tecnologica: "¿Qué es lo que se juega cuando Trump ordena cerrarnos el mail?"
+>>>>>>> Stashed changes
     },
     es: {
         description0: "Acá escribo sobre los proyectos en los que voy trabajando, lo que voy aprendiendo y cualquier otra cosa que me parezca interesante.",
@@ -17,7 +22,12 @@ const blogTranslations = {
         description3: "para que puedas abrirlo desde tu lector y enterarte de nuevas publicaciones.",
         time: "min de lectura",
         ezeluduena: "Creando una página web personal",
+<<<<<<< Updated upstream
         la_cosa: "La Cosa: Implementación del backend de un juego web"
+=======
+        la_cosa: "La Cosa: Implementación del backend de un juego web",
+        sob_tecnologica: "¿Qué es lo que se juega cuando Trump ordena cerrarnos el mail?"
+>>>>>>> Stashed changes
 
     }
 };
