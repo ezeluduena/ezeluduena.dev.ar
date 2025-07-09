0bfc61996452576b7803074e7ac2dc4fab46c4b4
Ezequiel Ludueña <ezeluduena123@gmail.com>
2025-07-09 16:48:28 -0300

    fix: blog locale

--- public/locale/blog.ts
+++ public/locale/blog.ts
@@ -1,35 +1,27 @@
 import { time } from "console";
 
 const blogTranslations = {
-    en: {
-        description0: "Here I write about the projects I'm working on, what I'm learning and anything else that I find interesting.",
-        description1: "You can suscribe to the blog's ",
-        description2: "RSS Feed",
-        description3: " to get notified when I post a new article.",
-        time: "min read",
-        ezeluduena: "Creating a personal website",
-<<<<<<< Updated upstream
-        la_cosa: "La Cosa: A web game backend development"
-=======
-        la_cosa: "La Cosa: A web game backend development",
-        sob_tecnologica: "¿Qué es lo que se juega cuando Trump ordena cerrarnos el mail?"
->>>>>>> Stashed changes
-    },
-    es: {
-        description0: "Acá escribo sobre los proyectos en los que voy trabajando, lo que voy aprendiendo y cualquier otra cosa que me parezca interesante.",
-        description1: "Este blog tiene disponible su ",
-        description2: "feed RSS ",
-        description3: "para que puedas abrirlo desde tu lector y enterarte de nuevas publicaciones.",
-        time: "min de lectura",
-        ezeluduena: "Creando una página web personal",
-<<<<<<< Updated upstream
-        la_cosa: "La Cosa: Implementación del backend de un juego web"
-=======
-        la_cosa: "La Cosa: Implementación del backend de un juego web",
-        sob_tecnologica: "¿Qué es lo que se juega cuando Trump ordena cerrarnos el mail?"
->>>>>>> Stashed changes
+        en: {
+                description0: "Here I write about the projects I'm working on, what I'm learning and anything else that I find interesting.",
+                description1: "You can suscribe to the blog's ",
+                description2: "RSS Feed",
+                description3: " to get notified when I post a new article.",
+                time: "min read",
+                ezeluduena: "Creating a personal website",
+                la_cosa: "La Cosa: A web game backend development",
+                sob_tecnologica: "¿Qué es lo que se juega cuando Trump ordena cerrarnos el mail?"
+        },
+        es: {
+                description0: "Acá escribo sobre los proyectos en los que voy trabajando, lo que voy aprendiendo y cualquier otra cosa que me parezca interesante.",
+                description1: "Este blog tiene disponible su ",
+                description2: "feed RSS ",
+                description3: "para que puedas abrirlo desde tu lector y enterarte de nuevas publicaciones.",
+                time: "min de lectura",
+                ezeluduena: "Creando una página web personal",
+                la_cosa: "La Cosa: Implementación del backend de un juego web",
+                sob_tecnologica: "¿Qué es lo que se juega cuando Trump ordena cerrarnos el mail?"
 
-    }
+        }
 };
 
 export default blogTranslations;
